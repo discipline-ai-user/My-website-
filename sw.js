fc72871d0796539129461a812dd5078717da29eb
discipline-ai-user <md1sajidra123@gmail.com>
2026-02-27 22:24:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-
+self.addEventListener("install", e => {
+ e.waitUntil(
+  caches.open("discipline-cache").then(cache => {
+   return cache.addAll([
+    "/",
+    "/index.html",
+    "/app.js",
+    "/ai.js",
+    "/stats.js",
+    "/sessions.js",
+    "/strict.js"
+   ]);
+  })
+ );
+});
